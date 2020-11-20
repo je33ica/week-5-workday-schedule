79bd73a98ace25f32ad3e59999e70796c1de2131
je33ica <je331687@gmail.com>
2020-11-20 17:25:14 +0000

    Javascript file added w/psudocode

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// get current day, date & time 
+//current day is shown at the top of the page
+
+// create schdule for standard business hours - defined as 9 AM > 5 PM ( checked offical way to display AM/PM)
+
+// row blocks of 1 hour with columns off time/input box /save
+
+// time blocks can be clicked
+//                     event added
+//                     event saved in local storage 
+
+
+// time blocks will need to be compared to current time |(if loop)
+// and then do something - change colour
+// past - 
+// present 
+// future
+
+// all needs to be dynamically created in javascript
